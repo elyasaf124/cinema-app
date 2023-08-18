f37b8c5b0610cd7e567a4fc2a4dc05748fe8ce77
elyasaf124 <elyasaf124@walla.com>
2023-08-18 12:14:45 +0300

    adding url

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -23,6 +23,7 @@ if (process.env.NODE_ENV === "development") {
   console.log("dev");
   webOrigin = [
     process.env.WEB_APP_URL_DEV as string,
+    process.env.WEB_APP_URL_DEV_ONE as string,
     process.env.WEB_APP_URL_DEV_TWO as string,
   ];
 } else if (process.env.NODE_ENV === "production") {
